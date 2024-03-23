2ba358b40db115f4b5803b3afe236320ef802ed6
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-03-23 00:40:43 +0000

    added line to print number of components extracted

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ n_samples, n_features = X.shape
 print('Number of samples:', n_samples)
 print('Number of features:', n_features)
 pca = PCA(2)
+print('Number of extracted features/principal components:', pca.n_components)
 pca.fit(X)
 print(pca.transform())
-
